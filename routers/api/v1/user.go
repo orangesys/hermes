df206ddb2fab8050c59d58a1cd7a2037725c5356
gavinzhou <gavin.zhou@gmail.com>
2019-07-08 15:42:32 +0900

    Added user api

--- routers/api/v1/user.go
+++ routers/api/v1/user.go
@@ -1,14 +1,18 @@
 package v1
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/orangesys/hermes/pkg/db"
+	"github.com/orangesys/hermes/pkg/payments"
 )
 
 // User is firebase database
 type User struct {
 	Email       string `json:"email" binding:"required,email"`
+	UserID      string `json:"userid" binding:"required"`
 	PlanID      string `json:"planid" binding:"required"`
 	CompanyName string `json:"companyname" binding:"required"`
 	CardNumber  string `json:"cardnumber" binding:"required"`
@@ -22,14 +26,48 @@ func CreateUser(c *gin.Context) {
 	if err := c.ShouldBindJSON(&u); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
-	// c.JSON(http.StatusOK, gin.H{"status": "signup successed"})
-	c.JSON(http.StatusOK, gin.H{
-		"email":       u.Email,
-		"companyName": u.CompanyName,
-		"planID":      u.PlanID,
-		"cardNumber":  u.CardNumber,
-		"expMonth":    u.ExpMonth,
-		"expYear":     u.ExpYear,
-		"cvc":         u.CVC,
-	})
+	// create customer with email, email is unique
+	cus, err := payments.CreateCustomer(u.CompanyName, u.Email)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"messages": err.Error(),
+		})
+		return
+	}
+	fmt.Println(cus.ID)
+	// add card source to customer
+	// cardNumber, expMonth, expYear, cvc, stripeCustomerID string
+	if _, err := payments.AddSource(u.CardNumber, u.ExpMonth, u.ExpYear, u.CVC, cus.ID); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"messages": err.Error(),
+		})
+		return
+	}
+
+	//add subscription to customer with planID
+	subItemID, err := payments.Addsubscription(u.PlanID, cus.ID)
+	fmt.Println(subItemID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"messages": err.Error(),
+		})
+		return
+	}
+
+	paymentData := map[string]interface{}{
+		"payments": map[string]interface{}{
+			"customerID":     cus.ID,
+			"subscriptionID": subItemID,
+		},
+	}
+
+	if err := db.UpdateDB(u.UserID, paymentData); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"messages": err.Error(),
+		})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"status": "signup successed"})
+	}
 }
